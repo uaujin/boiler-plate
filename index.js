a72ee3861acb45323126ecf14490a862cc37f40b
uaujin <0619kyj@naver.com>
2020-06-30 17:37:29 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://yujin:dbwls1452@boilerplate.cklju.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,useFindAndModify: false
+}).then(()=>console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => res.send('Hello World! 안뇽'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
